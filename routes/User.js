d65abc5dd22dea214231d2a670118af2d993b5d8
KastroF <nkastrro@gmail.com>
2025-11-21 15:52:57 +0100

    first commit

---
+++ routes/User.js
@@ -0,0 +1,17 @@
+const express = require("express"); 
+
+const router = express.Router(); 
+
+const userCtrl = require("../controllers/User"); 
+
+const auth = require("../middleware/auth");
+
+
+router.post("/onlogin", userCtrl.login);
+router.get("/getuser", auth, userCtrl.getUser)
+router.post("/updateservice", auth, userCtrl.updateService);
+router.post("/addservice", auth, userCtrl.addService);
+router.get("/getrecs", auth, userCtrl.getRecs);
+
+module.exports = router;
+
